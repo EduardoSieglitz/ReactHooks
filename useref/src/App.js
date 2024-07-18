09a250a2c8831c38ad0878f9318f5aa55c5c934c
EduardoSieglitz <eduardosieglitzdasilveira@gmail.com>
2024-07-18 19:36:27 -0300

    useRef com useState
    - Usando useState para deixar o programa mais completo .

--- useref/src/App.js
+++ useref/src/App.js
@@ -3,24 +3,30 @@ import { useEffect, useState } from "react";
 import { useRef } from "react";
 
 export default function Home() {
-  const [dados, setDados] = useState(),
+  const [nome, setNome] = useState(),
+    [email, setEmail] = useState(),
+    [telefone, setTelefone] = useState(),
     h2form = useRef(''),
     h2dados = useRef(''),
     nomeP = useRef(''),
     emailP = useRef(''),
     telefoneP = useRef('');
 
-  function getDados(evento) {
-    setDados(evento.target.value)
-    console.log(evento.target.value);
-  }
-
+    function getNome(evento){
+      setNome(evento.target.value);
+    }
+    function getEmail(evento){
+      setEmail(evento.target.value);
+    }
+    function getTelefone(evento){
+      setTelefone(evento.target.value);
+    }
   useEffect(() => {
-    h2form.current.innerHTML = 'Cadastro';
-    h2dados.current.innerHTML = 'Dados';
-    nomeP.current.innerHTML = "Nome Completo"; 
-    emailP.current.innerHTML = "Email";
-    telefoneP.current.innerHTML = "Telefone";
+    h2form.current.innerHTML = 'Cadastro:';
+    h2dados.current.innerHTML = 'Dados:';
+    nomeP.current.innerHTML = "Nome Completo:";
+    emailP.current.innerHTML = "Email:";
+    telefoneP.current.innerHTML = "Telefone:";
   }, []);
   return (
     <body>
@@ -32,12 +38,15 @@ export default function Home() {
             </div>
             <div className={styles.controlDados}>
               <p ref={nomeP}></p>
+              <p>{nome}</p>
             </div>
             <div className={styles.controlDados}>
               <p ref={emailP}></p>
+              <p>{email}</p>
             </div>
             <div className={styles.controlDados}>
               <p ref={telefoneP}></p>
+              <p>{telefone}</p>
             </div>
           </div>
         </div>
@@ -47,13 +56,13 @@ export default function Home() {
               <h2 ref={h2form}></h2>
             </div>
             <div className={styles.form}>
-              <input type="text" placeholder="Digite Nome completo" onChange={getDados} />
+              <input type="text" name="nome" placeholder="Digite Nome completo" onChange={getNome} />
             </div>
             <div className={styles.form}>
-              <input type="text" placeholder="Digite Email" onChange={getDados} />
+              <input type="text" name="email" placeholder="Digite Email" onChange={getEmail} />
             </div>
             <div className={styles.form}>
-              <input type="text" placeholder="Digite Telefone" onChange={getDados} />
+              <input type="text" name="telefone" placeholder="Digite Telefone" onChange={getTelefone} />
             </div>
             <div className={styles.form}>
               <button>Cadastrar</button>
