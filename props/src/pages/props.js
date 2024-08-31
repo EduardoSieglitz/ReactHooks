b3d8532762867fe88fbf0cd808999fdeb593c8f6
EduardoSieglitz <eduardosieglitzdasilveira@gmail.com>
2024-08-31 18:35:49 -0300

    Update props.js

--- props/src/pages/props.js
+++ props/src/pages/props.js
@@ -2,8 +2,8 @@ import './props.css';
 import Props from "../Message.js/getProps";
 
 function Home() {
-  const Message = <Props></Props>;
-  
+  const Message = <Props ></Props>;
+  console.log(Message)
   return (
     <div className="container">
       <p>{Message}</p>
